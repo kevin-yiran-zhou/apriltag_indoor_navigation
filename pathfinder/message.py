6b5b92a909a78d27d590adc7b56015861553cb04
kevin-yiran-zhou <yiranzhou93@gmail.com>
2024-10-25 16:53:34 -0400

    direction fixed

--- pathfinder/message.py
+++ pathfinder/message.py
@@ -47,8 +47,7 @@ def generate_directions(user_pose, path, scale=1.0):
         direction_vector = next_point - current_pose
         angle_to_next_point = np.degrees(np.arctan2(direction_vector[1], direction_vector[0]))
 
-        # relative_angle = angle_to_next_point - current_orientation # correct
-        relative_angle = angle_to_next_point + current_orientation # incorrect
+        relative_angle = angle_to_next_point - current_orientation
         distance_to_next_point = np.linalg.norm(direction_vector) * scale
 
         # Create a message for the current step
