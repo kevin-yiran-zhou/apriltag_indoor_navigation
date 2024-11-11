58f772c926596652ae4c62f9877ad7bb5a6bf3da
kevin-yiran-zhou <yiranzhou93@gmail.com>
2024-11-11 14:32:03 -0500

    1111

--- room_label_detector/tess_tune.py
+++ room_label_detector/tess_tune.py
@@ -1,7 +1,6 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 
-# Load the image
 image_path = '/home/kevinbee/Desktop/apriltag_indoor_navigation/room_label_detector/images/office_num.png'
 image = Image.open(image_path)
 
@@ -13,8 +12,6 @@ threshold_image = enhanced_image.point(lambda p: p > 75 and 255)
 sharpened_image = threshold_image.filter(ImageFilter.SHARPEN)
 sharpened_image.save('/home/kevinbee/Desktop/apriltag_indoor_navigation/room_label_detector/images/office_num_sharpened.png')
 
-# Use OCR with different PSM
+# OCR
 extracted_text = pytesseract.image_to_string(sharpened_image, config='--oem 3 --psm 6 outputbase digits')
-
-# Print the extracted text
 print("Extracted Text:", extracted_text)
--- room_label_detector/tesseract_detector.py
+++ room_label_detector/tesseract_detector.py
@@ -4,16 +4,13 @@ import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
 def detect_number(image, corners):
-    # Unpack the four corners
     top_right, top_left, bottom_left, bottom_right = corners
-    
-    # Define the width and height of the new perspective rectangle
+
     width = max(np.linalg.norm(np.array(top_right) - np.array(top_left)),
                 np.linalg.norm(np.array(bottom_right) - np.array(bottom_left)))
     height = max(np.linalg.norm(np.array(top_left) - np.array(bottom_left)),
                  np.linalg.norm(np.array(top_right) - np.array(bottom_right)))
     
-    # Destination points for the perspective transformation
     dst_points = np.array([
         [0, 0],
         [width - 1, 0],
@@ -21,28 +18,29 @@ def detect_number(image, corners):
         [0, height - 1]
     ], dtype="float32")
     
-    # Source points from the corners
     src_points = np.array([top_left, top_right, bottom_right, bottom_left], dtype="float32")
     
-    # Perform perspective transformation
     matrix = cv2.getPerspectiveTransform(src_points, dst_points)
     warped = cv2.warpPerspective(image, matrix, (int(width), int(height)))
 
-    # OCR on the warped image
+    # OCR
     image = Image.fromarray(warped)
     gray_image = image.convert('L')
     enhancer = ImageEnhance.Contrast(gray_image)
     enhanced_image = enhancer.enhance(2)
     threshold_image = enhanced_image.point(lambda p: p > 75 and 255)
     sharpened_image = threshold_image.filter(ImageFilter.SHARPEN)
-    text = pytesseract.image_to_string(sharpened_image, config='--oem 3 --psm 6 outputbase digits')
+    ## Detect only digits
+    # text = pytesseract.image_to_string(sharpened_image, config='--oem 3 --psm 6 outputbase digits')
+    # Detect digits and letters
+    text = pytesseract.image_to_string(sharpened_image, config='--oem 3 --psm 6')
+
     text = text.replace(" ", "").replace("\n", "")
     return text
 
 
-def detect_room_label(image_path, resize_factor=5, area_threshold=100, approx_tolerance=0.05):
-    # Load and resize the image
-    image = cv2.imread(image_path)
+def detect_room_label(image, resize_factor=5, area_threshold=100, approx_tolerance=0.1):
+    # Resize the image
     image = cv2.resize(image, (image.shape[1] // resize_factor, image.shape[0] // resize_factor))
     
     # Apply bilateral filter for noise reduction while keeping edges sharp
@@ -65,39 +63,32 @@ def detect_room_label(image_path, resize_factor=5, area_threshold=100, approx_to
     # Find contours
     contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    rectangle_corners = []  # To store the four corners of each detected rectangle
+    rectangle_corners = []
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
         
-        # Only consider large enough areas to filter noise
         if area > area_threshold:
-            # Approximate the contour to simplify it
             approx = cv2.approxPolyDP(cnt, approx_tolerance * cv2.arcLength(cnt, True), True)
-            
-            # Check if the contour has 4 vertices (rectangle-like shape)
             if len(approx) == 4:
-                
-                # Extract the corner points
                 corners = [point[0] for point in approx]
                 rectangle_corners.append(corners)
 
                 OCR_result = detect_number(image, corners)
-                if sum(char.isdigit() for char in OCR_result) >= 2:
-                    print("Detected Number:", OCR_result)
+
+                # if sum(char.isdigit() for char in OCR_result) >= 2:
+                if len(OCR_result) >= 4:
                     cv2.drawContours(image, [approx], -1, (0, 255, 0), 3)
                     cv2.putText(image, OCR_result, tuple(corners[0]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                     the_corners = corners
                     the_number = OCR_result
                     # break
     
-    # Display the image with detected rectangles for visualization
     cv2.imshow('Rectangles Detected', image)
     cv2.waitKey(20000)
     cv2.destroyAllWindows()
 
     return the_corners, the_number
 
-# Example usage
-image_path = "/home/kevinbee/Desktop/apriltag_indoor_navigation/room_label_detector/images/office.JPG"
-corners, number = detect_room_label(image_path, resize_factor=4, area_threshold=500, approx_tolerance=0.05)
+image = cv2.imread("/home/kevinbee/Desktop/apriltag_indoor_navigation/room_label_detector/images/office.JPG")
+corners, number = detect_room_label(image, resize_factor=4, area_threshold=500, approx_tolerance=0.05)
